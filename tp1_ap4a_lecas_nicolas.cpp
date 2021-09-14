59d906939b0f2efd4c2da836536da2666ef8e78b
Nicolas LECAS <nlecas@pc-gi-152.utbm.local>
2021-09-14 12:17:40 +0200

    Programme Hello world

---
+++ tp1_ap4a_lecas_nicolas.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : tp1_ap4a_lecas_nicolas.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main() {
+	std::cout << "!!!Hello World!!!";
+	return 0;
+}

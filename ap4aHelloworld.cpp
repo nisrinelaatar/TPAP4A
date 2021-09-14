e17450bd1f177610456bc289b31cbeabfbd4697b
Gökdeniz ALBUZLU <galbuzlu@pc-gi-153.utbm.local>
2021-09-14 12:17:45 +0200

    ajout du fichier hello world

---
+++ ap4aHelloworld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ap4a.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}

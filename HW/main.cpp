4ca8c60a432effd759163d084437805ee6241d41
visclo44 <64807821+visclo44@users.noreply.github.com>
2021-09-14 12:02:54 +0200

    hello world

---
+++ HW/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 14 sept. 2021
+ *      Author: clovi
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello World"<< endl;
+
+	return 0;
+}
+
+
